3d72b33f021c20d94e4c0b4101adbbb23884f32e
Prince-Addo <karimprinceaddo@yahoo.com>
2026-06-06 18:45:06 -0600

    fix: use released mediasoup Apple client tag

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/MediaSFU/mediasfu-mediasoup-client-apple.git", from: "1.0.0")
+        .package(url: "https://github.com/MediaSFU/mediasfu-mediasoup-client-apple.git", from: "0.1.0")
     ],
     targets: [
         .target(
